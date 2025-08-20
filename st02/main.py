81b24526d37e7cfd6f17c34c957487576b5e7598
h <hyeju_44@naver.com>
2025-08-20 11:09:54 +0900

    20250820

--- st02/main.py
+++ st02/main.py
@@ -1,3 +1,4 @@
 import streamlit as st
 
 st.markdown("Main Page")
+st.sidebar.markdown("Clicked Main Page")
--- st02/p1.py
+++ st02/p1.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
+#Sidebar
+st.sidebar.markdown("Clicked Page1")
+st.sidebar.markdown("Side Menu")
+st.sidebar.button("Click")
+st.sidebar.radio("성별", ["여자", "남자"])
+
+#Title
 st.title("Page 1")
+st.markdown("Mark Down")
+st.header("Header")
+st.subheader("Sub Header")
+st.caption("Caption")
+st.code("""
+    def func():
+        print("OK")
+    def func2():
+        print("OOOOOOOOOOO")
+ """, language="python")
